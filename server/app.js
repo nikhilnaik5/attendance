fc233cd0ee57b7709c11bc22e6cc4f0c8485ccae
nikhilnaik5 <82027742+nikhilnaik5@users.noreply.github.com>
2023-01-14 13:22:23 +0530

    Modified scripts

--- server/app.js
+++ server/app.js
@@ -5,14 +5,14 @@ const cors = require("cors")
 const bodyParser = require('body-parser');
 const jwt = require('jsonwebtoken');
 const app = express();
-
+const path = require('path');
 const authRoute = require('./router/auth');
 const lectureRoute = require('./router/lecture');
 app.use(cors());
 app.use('/static', express.static('static'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(express.static(path.join(__dirname, "..", "client", "build")));
 
 
 dotenv.config({path:'./config.env'});
@@ -23,6 +23,10 @@ require('./config/passjwt');
 
 app.use("/auth", authRoute);
 app.use("/lecture", lectureRoute);
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "..", "client", "build", "index.html"));
+})
 
 app.listen(port, ()=>{
     console.log("Server started! at "+port);
